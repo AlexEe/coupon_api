355879732016bc9fecbd05169905aa8f5bb5f600
AlexEe <alexee@alexandras-mbp.openvpn>
2019-07-04 11:06:01 +0100

    Fixed error when printing example coupon

--- main.go
+++ main.go
@@ -7,46 +7,35 @@ import (
 )
 
 type Datastore interface {
-	GetCoupons() ([]Coupon, error)
-	GetCoupon(int) (Coupon, error)
-	AddCoupon(Coupon) (int, error)
-	UpdateCoupon(int, Coupon) (bool, error)
+	GetCoupons() []Coupon
+	// GetCoupon(int) Coupon
+	// AddCoupon(Coupon) int
+	// UpdateCoupon(int, Coupon) (bool, error)
 }
 
+// no filepath but pointer to databasedriver
 type ActualDataStore struct {
 	db *sql.DB
 }
 
-func (fb *FakeDatastore) UpdateCoupon(couponID int, coupon Coupon) (bool, error) {
-	return true, nil
-}
+// func (db *ActualDataStore) UpdateCoupon(couponID int, coupon Coupon) (bool, error) {
+// 	// does a bunch of MySQL shit
+// 	return true, nil
+// }
 
-func (fb *FakeDatastore) AddCoupon(coupon Coupon) (int, error) {
-	return 0, nil
-}
+// func (db *ActualDataStore) AddCoupon(coupon Coupon) (int, error) {
+// 	// does a bunch of MySQL shit
+// 	return 0, nil
+// }
 
-func (fb *FakeDatastore) GetCoupon(CouponID int) (Coupon, error) {
-	return Coupon{}, nil
-}
+// func (db *ActualDataStore) GetCoupon(CouponID int) (Coupon, error) {
+// 	// does a bunch of MySQL shit
+// 	return Coupon{}, nil
+// }
 
-func (fb *FakeDatastore) GetCoupons() ([]Coupon, error) {
-	return nil, nil
-}
-
-func (db *ActualDataStore) UpdateCoupon(couponID int, coupon Coupon) (bool, error) {
-	return true, nil
-}
-
-func (db *ActualDataStore) AddCoupon(coupon Coupon) (int, error) {
-	return 0, nil
-}
-
-func (db *ActualDataStore) GetCoupon(CouponID int) (Coupon, error) {
-	return Coupon{}, nil
-}
-
-func (db *ActualDataStore) GetCoupons() ([]Coupon, error) {
-	return nil, nil
+func (db *ActualDataStore) GetCoupons() []Coupon {
+	// does a bunch of MySQL shit
+	return nil
 }
 
 type Coupon struct {
@@ -63,23 +52,22 @@ type CouponRetriever struct {
 }
 
 func (c *CouponRetriever) ListCoupons() []Coupon {
-	coupons, _ := c.db.GetCoupons()
-	return coupons
+	return c.db.GetCoupons()
 }
 
 type FakeDatastore struct {
 	Coupons []Coupon
 }
 
-// func (f *FakeDatastore) GetCoupons() []Coupon {
-// 	return f.Coupons
-// }
+func (f *FakeDatastore) GetCoupons() []Coupon {
+	return f.Coupons
+}
 
 type Handler struct {
 	Coupons *CouponRetriever
 }
 
-func (h *Handler) HandleCoupons(res http.ResponseWriter, req *http.Request) {
+func (h *Handler) ListCoupons(res http.ResponseWriter, req *http.Request) {
 	data := h.Coupons.ListCoupons()
 	res.Header().Set("Content-Type", "application/json")
 	res.WriteHeader(http.StatusCreated)
@@ -90,7 +78,6 @@ func main() {
 	// create a user in mysql to access database
 	// db, _ := sql.Open("mysql", "root:password1@tcp(127.0.0.1:3306)/coupons")
 	// actualDS := &ActualDataStore{db}
-	// db := &ActualDataStore{}
 
 	couponsList := []Coupon{
 		Coupon{
@@ -103,9 +90,9 @@ func main() {
 	}
 
 	fakeDb := &FakeDatastore{couponsList}
-
-	couponRetriever := &CouponRetriever{fakeDb}
-	h := &Handler{couponRetriever}
-	http.HandleFunc("/coupon", h.HandleCoupons)
+	// db := &ActualDataStore{}
+	Coupons := &CouponRetriever{fakeDb}
+	h := &Handler{Coupons}
+	http.HandleFunc("/coupon", h.ListCoupons)
 	http.ListenAndServe(":8080", nil)
 }
