10ed37e31ab1503e38f2bef04b597a9dd85fbed8
AlexEe <alexee@Alexandras-MacBook-Pro.local>
2019-07-26 12:09:00 +0100

    Fix bug in when getting coupons by value

--- pkg/coupon/listone.go
+++ pkg/coupon/listone.go
@@ -6,8 +6,8 @@ import (
 	"os"
 )
 
-// GetSingleCoupon retrieves a single coupon by name
-func GetSingleCoupon(name string) Coupon {
+// GetSingleCoupon retrieves a single coupon
+func GetSingleCoupon(column string, content string) Coupon {
 	var cp Coupon
 
 	db, err := db.Open()
@@ -17,7 +17,28 @@ func GetSingleCoupon(name string) Coupon {
 	}
 	defer db.Close()
 
-	query := fmt.Sprintf("SELECT * FROM coupons WHERE name = '%v'", name)
+	query := fmt.Sprintf("SELECT * FROM coupons WHERE %v = '%v'", column, content)
+
+	err = db.QueryRow(query).Scan(&cp.ID, &cp.Name, &cp.Brand, &cp.Value, &cp.CreatedAt, &cp.Expiry)
+	if err != nil {
+		fmt.Println("Error getting coupon from database:", err)
+		os.Exit(1)
+	}
+	return cp
+}
+
+// GetSingleCoupon retrieves a single coupon by value which is a float, not string
+func GetSingleCouponByValue(column string, content float64) Coupon {
+	var cp Coupon
+
+	db, err := db.Open()
+	if db == nil || err != nil {
+		fmt.Println("Error opening database:", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	query := fmt.Sprintf("SELECT * FROM coupons WHERE %v = '%v'", column, content)
 
 	err = db.QueryRow(query).Scan(&cp.ID, &cp.Name, &cp.Brand, &cp.Value, &cp.CreatedAt, &cp.Expiry)
 	if err != nil {
--- pkg/http/handler/handler.go
+++ pkg/http/handler/handler.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"strconv"
 )
 
@@ -16,45 +17,85 @@ func GetAllCoupons(res http.ResponseWriter, req *http.Request) {
 }
 
 func GetSingleCoupon(res http.ResponseWriter, req *http.Request) {
-	name, ok := req.URL.Query()["name"]
-	if !ok {
-		fmt.Println("Error: Missing parameter 'name'.")
+	var data coupon.Coupon
+
+	valueString, ok := req.URL.Query()["value"]
+	if ok {
+		value, _ := strconv.ParseFloat(valueString[0], 32)
+		data = coupon.GetSingleCouponByValue("Value", value)
+		res.Header().Set("Content-Type", "application/json")
+		res.WriteHeader(http.StatusCreated)
+		json.NewEncoder(res).Encode(data)
 	}
-	data := coupon.GetSingleCoupon(name[0])
-	res.Header().Set("Content-Type", "application/json")
-	res.WriteHeader(http.StatusCreated)
-	json.NewEncoder(res).Encode(data)
+
+	name, ok := req.URL.Query()["name"]
+	if ok {
+		data = coupon.GetSingleCoupon("name", name[0])
+		res.Header().Set("Content-Type", "application/json")
+		res.WriteHeader(http.StatusCreated)
+		json.NewEncoder(res).Encode(data)
+	}
+
+	brand, ok := req.URL.Query()["brand"]
+	if ok {
+		data = coupon.GetSingleCoupon("brand", brand[0])
+		res.Header().Set("Content-Type", "application/json")
+		res.WriteHeader(http.StatusCreated)
+		json.NewEncoder(res).Encode(data)
+	}
+
+	created, ok := req.URL.Query()["created"]
+	if ok {
+		data = coupon.GetSingleCoupon("created", created[0])
+		res.Header().Set("Content-Type", "application/json")
+		res.WriteHeader(http.StatusCreated)
+		json.NewEncoder(res).Encode(data)
+	}
+
+	expiry, ok := req.URL.Query()["expiry"]
+	if ok {
+		data = coupon.GetSingleCoupon("expiry", expiry[0])
+		res.Header().Set("Content-Type", "application/json")
+		res.WriteHeader(http.StatusCreated)
+		json.NewEncoder(res).Encode(data)
+	}
+
 }
 
 func AddCoupon(res http.ResponseWriter, req *http.Request) {
 	name, ok := req.URL.Query()["name"]
 	if !ok {
 		fmt.Println("Error: Missing parameter 'name'.")
+		os.Exit(1)
 	}
 	// io.WriteString(res, name[0])
 
-	branch, ok := req.URL.Query()["branch"]
+	brand, ok := req.URL.Query()["brand"]
 	if !ok {
-		fmt.Println("Error: Missing parameter 'branch'.")
+		fmt.Println("Error: Missing parameter 'brand'.")
+		os.Exit(1)
 	}
 
 	valueString, ok := req.URL.Query()["value"]
 	if !ok {
-		fmt.Println("Error: Missing parameter 'branch'.")
+		fmt.Println("Error: Missing parameter 'value'.")
+		os.Exit(1)
 	}
 	value, _ := strconv.ParseFloat(valueString[0], 32)
 
 	created, ok := req.URL.Query()["created"]
 	if !ok {
-		fmt.Println("Error: Missing parameter 'branch'.")
+		fmt.Println("Error: Missing parameter 'created'.")
+		os.Exit(1)
 	}
 
 	expiry, ok := req.URL.Query()["expiry"]
 	if !ok {
-		fmt.Println("Error: Missing parameter 'branch'.")
+		fmt.Println("Error: Missing parameter 'expiry'.")
+		os.Exit(1)
 	}
 
-	data := coupon.Add(name[0], branch[0], value, created[0], expiry[0])
+	data := coupon.Add(name[0], brand[0], value, created[0], expiry[0])
 	res.Header().Set("Content-Type", "application/json")
 	res.WriteHeader(http.StatusCreated)
 	json.NewEncoder(res).Encode(data)
